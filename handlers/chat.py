0bdb84580b467064ea080f1f64aa72cfd14844fc
alcyada <89109873+alcyada@users.noreply.github.com>
2022-04-06 17:51:45 +0530

    Update chat.py

--- handlers/chat.py
+++ handlers/chat.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-
+from main.py import KUKI_API
 
 @Client.on_message(
     filters.text
@@ -13,7 +13,7 @@ async def kukiai(client: Client, message: Message):
   msg = message.text
   chat_id = message.chat.id
 
-  Kuki =   requests.get(f"https://kukiapi.xyz/api/message={msg}").json()
+  Kuki =   requests.get(f"https://kukiapi.xyz/api/apikey={KUKI_API}/message={msg}").json()
 
   moezilla = f"{Kuki['reply']}"
 
@@ -21,6 +21,39 @@ async def kukiai(client: Client, message: Message):
   bot_id = self.id
   if not message.reply_to_message.from_user.id == bot_id:
          return
+      
+  await client.send_chat_action(message.chat.id, "typing")
+  await message.reply_text(moezilla)
+
+
+@Client.on_message(
+    filters.text
+    & ~filters.reply
+    & filters.private
+    & ~filters.bot
+    & ~filters.edited,
+    group=2,
+)
+async def kukiai(client: Client, message: Message):
+  msg = message.text
+  chat_id = message.chat.id
+
+  Kuki =   requests.get(f"https://kukiapi.xyz/api/apikey={KUKI_API}/message={msg}").json()
+
+  moezilla = f"{Kuki['reply']}"
+      
+  await client.send_chat_action(message.chat.id, "typing")
+  await message.reply_text(moezilla)
+
+@Client.on_message(
+    filters.command("chat", prefixes=["/", ".", "?", "-"]))
+async def kukiai(client: Client, message: Message):
+
+  msg = message.text.replace(message.text.split(" ")[0], "")
+    
+  Kuki =   requests.get(f"https://kukiapi.xyz/api/apikey={KUKI_API}/message={msg}").json()
+
+  moezilla = f"{Kuki['reply']}"
       
   await client.send_chat_action(message.chat.id, "typing")
   await message.reply_text(moezilla)
