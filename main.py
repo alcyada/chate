ce151ecaf874efa81c63bc3837a92aab0914f3d5
alcyada <89109873+alcyada@users.noreply.github.com>
2022-04-06 17:53:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,68 +19,9 @@ bot = Client(
 
 
 
-@bot.on_message(
-    filters.text
-    & filters.reply
-    & ~filters.private
-    & ~filters.bot
-    & ~filters.edited,
-    group=2,
-)
-async def kukiai(client: Client, message: Message):
-  msg = message.text
-  chat_id = message.chat.id
-
-  Kuki =   requests.get(f"https://kukiapi.xyz/api/message={msg}").json()
-
-  moezilla = f"{Kuki['reply']}"
-
-  self = await bot.get_me()
-  bot_id = self.id
-  if not message.reply_to_message.from_user.id == bot_id:
-         return
-      
-  await client.send_chat_action(message.chat.id, "typing")
-  await message.reply_text(moezilla)
-
-
-@bot.on_message(
-    filters.text
-    & ~filters.reply
-    & filters.private
-    & ~filters.bot
-    & ~filters.edited,
-    group=2,
-)
-async def kukiai(client: Client, message: Message):
-  msg = message.text
-  chat_id = message.chat.id
-
-  Kuki =   requests.get(f"https://kukiapi.xyz/api/apikey={KUKI_API}/message={msg}").json()
-
-  moezilla = f"{Kuki['reply']}"
-      
-  await client.send_chat_action(message.chat.id, "typing")
-  await message.reply_text(moezilla)
-
-
-@bot.on_message(
-    filters.command("chat", prefixes=["/", ".", "?", "-"]))
-async def kukiai(client: Client, message: Message):
-
-  msg = message.text.replace(message.text.split(" ")[0], "")
-    
-  Kuki =   requests.get(f"https://kukiapi.xyz/api/apikey={KUKI_API}/message={msg}").json()
-
-  moezilla = f"{Kuki['reply']}"
-      
-  await client.send_chat_action(message.chat.id, "typing")
-  await message.reply_text(moezilla)
 
 
 
-
-
-
-bot.run()
+bot.start()
+idle()
 
