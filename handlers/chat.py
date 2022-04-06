be49614ef93ba7bf58dfd0dc06c0fcb7a8575cb4
alcyada <89109873+alcyada@users.noreply.github.com>
2022-04-06 17:48:29 +0530

    Update chat.py

--- handlers/chat.py
+++ handlers/chat.py
@@ -1,1 +1,26 @@
 from pyrogram import Client, filters
+
+
+@Client.on_message(
+    filters.text
+    & filters.reply
+    & ~filters.private
+    & ~filters.bot
+    & ~filters.edited,
+    group=2,
+)
+async def kukiai(client: Client, message: Message):
+  msg = message.text
+  chat_id = message.chat.id
+
+  Kuki =   requests.get(f"https://kukiapi.xyz/api/message={msg}").json()
+
+  moezilla = f"{Kuki['reply']}"
+
+  self = await bot.get_me()
+  bot_id = self.id
+  if not message.reply_to_message.from_user.id == bot_id:
+         return
+      
+  await client.send_chat_action(message.chat.id, "typing")
+  await message.reply_text(moezilla)
